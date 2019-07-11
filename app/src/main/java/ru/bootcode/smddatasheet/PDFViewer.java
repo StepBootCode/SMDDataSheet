b81ccf98d108d17d3e829af1306f60512923cb49
StepBootCode <snstepchenkov@mail.ru>
2019-07-11 16:55:28 +0300

    update 11/07/2019

--- app/src/main/java/ru/bootcode/smddatasheet/PDFViewer.java
+++ app/src/main/java/ru/bootcode/smddatasheet/PDFViewer.java
@@ -4,7 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
 import com.joanzapata.pdfview.PDFView;
 
 import java.io.File;
@@ -14,6 +19,11 @@ public class PDFViewer extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_pdfviewer);
 
         PDFView pdfView = findViewById(R.id.pdfview);
@@ -23,6 +33,17 @@ public class PDFViewer extends AppCompatActivity {
 
         pdfView.fromFile(new File(filename))
                 .load();
+
+        // Рекламма, без нее ни как :) -------------------------------------------------------------
+        // Надо подумать как добавить китайскую сеть Youmi
+        AdView mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+        mAdView.setAdListener(new AdListener() {
+            @Override
+            public void onAdFailedToLoad(int errorCode) {
+            }
+        });
     }
 
 }
